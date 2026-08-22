39b7d724a2a2c56bf8085d33498aa9d4c44c216f
Phirimhel <118508698+Phirimhel@users.noreply.github.com>
2026-08-22 14:39:00 +0300

    Add: two-integer-sum-ii - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-10.go
@@ -0,0 +1,28 @@
+func twoSum(numbers []int, target int) []int {
+	output := make([]int, 2)
+
+	left := 0 
+	right := len(numbers)-1 
+	for left < right {
+
+		sum := numbers[left] + numbers[right]
+
+		if sum == target {
+			output[0], output[1] = left + 1, right + 1
+			break
+		}
+
+		if sum > target { 
+			right--
+			continue
+		}
+
+		if sum < target { 
+			left++
+			continue
+		}
+
+	}
+
+	return output
+}
