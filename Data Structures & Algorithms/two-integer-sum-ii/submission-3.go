6285982f68da59cd169bb48d8c59ea994ad81387
Phirimhel <118508698+Phirimhel@users.noreply.github.com>
2026-08-22 14:16:48 +0300

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.go
@@ -0,0 +1,18 @@
+func twoSum(numbers []int, target int) []int {
+
+	find := make(map[int]int, len(numbers))
+	res := make([]int, 2)
+
+	for i := range numbers {
+		remainder := target - numbers[i]
+
+		if j, ok := find[numbers[i]]; ok {
+			res[0], res[1] = j, (i+1)
+		}
+
+		find[remainder] = (i + 1) 
+
+	}
+
+	return res
+}
