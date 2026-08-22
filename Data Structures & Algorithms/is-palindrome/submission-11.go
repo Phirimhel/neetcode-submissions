c094ea12fcc4c2db993757e6a86dbde3bf74b11d
Phirimhel <118508698+Phirimhel@users.noreply.github.com>
2026-08-22 12:10:35 +0300

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.go
@@ -0,0 +1,35 @@
+func isPalindrome(s string) bool {
+	
+    n := len(s)
+	if n == 1 {
+		return true
+	}
+
+
+    s = strings.ToLower(s)
+
+    left := 0
+    right := n-1 
+
+    for left <= right { 
+        if !isAlphanumeric(s[left]) {
+            left++
+            continue
+        }
+
+        if !isAlphanumeric(s[right]) {
+            right--
+            continue
+        }
+
+        if s[left] != s[right] {
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+}
+func isAlphanumeric(b byte) bool {
+    return (b >= 'a' && b <='z') || (b >= '0' && b <='9')
+}
