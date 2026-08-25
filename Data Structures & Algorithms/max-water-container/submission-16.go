241b744e030f54bcb74550366e8f277f9520d42b
Phirimhel <118508698+Phirimhel@users.noreply.github.com>
2026-08-26 00:56:14 +0300

    Add: max-water-container - submission-16

---
+++ Data Structures & Algorithms/max-water-container/submission-16.go
@@ -0,0 +1,29 @@
+func maxArea(heights []int) int {
+	i := 0
+	j := len(heights) - 1
+
+	maxWaterLevel := 0
+	for i < j {
+		
+		newWaterLevel := min(heights[i], heights[j]) * (j - i)
+		if newWaterLevel > maxWaterLevel {
+			maxWaterLevel = newWaterLevel
+		}
+
+		if heights[i] > heights[j] {
+			j-- 
+		} else {
+			i++
+		}
+	}
+	return maxWaterLevel
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a 
+	} 
+	return b
+}
+
+
