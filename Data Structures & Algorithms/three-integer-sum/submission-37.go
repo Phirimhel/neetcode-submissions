b50260e706867c582b78e135e4cf0714c5b07b27
Phirimhel <118508698+Phirimhel@users.noreply.github.com>
2026-08-26 11:49:33 +0300

    Add: three-integer-sum - submission-37

---
+++ Data Structures & Algorithms/three-integer-sum/submission-37.go
@@ -0,0 +1,30 @@
+func threeSum(nums []int) [][]int {
+	output := make([][]int, 0, len(nums))
+
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums); i++ {
+		if i > 0 && nums[i-1] == nums[i] {
+			continue
+		}
+
+		for j := i + 1; j < len(nums); j++ {
+			if j > i + 1 && nums[j-1] == nums[j] {
+				continue
+			}
+
+			for k := j + 1; k < len(nums); k++ {
+				if k > j + 1 && nums[k-1] == nums[k] {
+					continue
+				}
+				
+				if nums[i] + nums[j] + nums[k] == 0 {
+					output = append(output, []int{nums[i], nums[j],nums[k]})
+				}
+			}
+		} 
+
+	}
+
+	return output
+}
