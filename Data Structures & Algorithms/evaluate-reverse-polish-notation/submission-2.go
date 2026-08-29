da168d2fb96862465be491c6147b63bcebd5bf58
Phirimhel <118508698+Phirimhel@users.noreply.github.com>
2026-08-29 17:56:06 +0300

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.go
@@ -0,0 +1,47 @@
+func evalRPN(tokens []string) int {
+
+	stack := make([]int, 0)
+
+	for _, token := range tokens {
+		
+		if token == "+" || 
+			token == "-" || 
+			token == "*" || 
+			token == "/" {
+
+			n := len(stack) - 1
+
+			result := arithmeticOperation(
+				stack[n-1],
+				stack[n],
+				token,
+			)
+
+			stack[n-1] = result
+			stack = stack[:n]
+		} else {
+			num, _ := strconv.Atoi(token)
+			stack = append(stack, num)
+		}
+	}
+
+	return stack[0]
+}
+
+
+func arithmeticOperation(x int, y int, operator string) int {
+	result := 0
+	switch {
+		case operator == "+":
+			return x + y
+		case operator == "-":
+			return x - y
+		case operator == "*":
+			return x * y
+		case operator == "/":
+			return x / y
+	}
+
+	return result
+
+}
